b15b3b920ff0faa5dccedb2781e14184efed4568
EkaterinaTadueva <etadueva@bk.ru>
2022-12-02 17:40:15 +0300

    Дописала js для открытия карточек при клике

--- source/js/modules/tuch-event.js
+++ source/js/modules/tuch-event.js
@@ -1,8 +1,22 @@
-let button = document.querySelector('.form__button');
+const buttons = document.querySelectorAll('.catalog__button-cover');
+const itemsContent = document.querySelectorAll('.catalog__item-content');
 
 const showCard = () => {
-  button.addEventListener('tuchstart', () => {
-  }, {passive: true});
+  for (let i = 0; i <= buttons.length; i++) {
+    let indexButton = i;
+    let button = buttons[i];
+    button.addEventListener('click', () => {
+      for (let j = 0; j <= itemsContent.length; j++) {
+        let indexContent = j;
+        let itemContent = itemsContent[j];
+        if (indexButton === indexContent) {
+          itemContent.classList.add('catalog__item-content--max-index');
+        } else {
+          itemContent.classList.remove('catalog__item-content--max-index');
+        }
+      }
+    }, {passive: true});
+  }
 };
 
 export {showCard};
